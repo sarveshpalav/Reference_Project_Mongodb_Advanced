2d509deddfca59601c52bc70082ccf1b0949afce
Sarvesh Palav <sarveshpalav@gmail.com>
2018-06-23 02:35:12 +0530

    Reference Project for Adavance Mongodb Queries

---
+++ database/models/album.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const AlbumSchema = new Schema ({
+
+    title:String,
+    date:Date,
+    copiesSold:Number,
+    numberTracks:Number,
+    image:String,
+    revenue:Number
+
+});
+
+
+module.exports = AlbumSchema;
